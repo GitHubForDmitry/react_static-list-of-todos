da899dd9113840522643e71f21ba96724685fbe5
Dmitry Koka <koka.d.wezom@gmail.com>
2020-09-09 23:03:35 +0300

    fastforward

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 import './App.css';
-
+// or less ideally
+import { Button } from 'react-bootstrap'
 import todos from './api/todos';
 import users from './api/users';
 
 function App() {
+  // userId: 1,
+  //   id: 1,
+  //   title: 'delectus aut autem',
+  //   completed: false,
+  const allUsers = todos.map(item => {return (
+    {
+      userId: item.userId,
+      id: item.id,
+      title: item.title,
+      completed: item.completed,
+      users: users.find(user => user.id === item.userId)
+
+    }
+  )
+
+  })
+
+  const onchange = (e) => console.log(e)
   return (
     <div className="App">
       <h1>Static list of todos</h1>
@@ -17,6 +36,24 @@ function App() {
         <span>Users: </span>
         {users.length}
       </p>
+      <div className="container">
+        {allUsers.map(item => { return (
+          <div className='container__block' key={item.id}>
+          <div>{item.id}</div>
+          <div>{item.title}</div>
+          <div>{item.users.name}</div>
+
+            {item.completed ?
+            <input type="checkbox" checked onChange={event => onchange(event.target.value)}/>
+              :
+              <input type="checkbox" onChange={event => onchange(event.target.value)}/>
+            }
+
+          </div>
+          )})}
+
+      </div>
+
     </div>
   );
 }
