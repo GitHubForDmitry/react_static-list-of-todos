9c51050df96c0b37dba2c6b73a8716233ec12d02
Dmitry Koka <koka.d.wezom@gmail.com>
2020-09-10 23:02:23 +0300

    step2

--- src/App.js
+++ src/App.js
@@ -5,6 +5,18 @@ import todos from './api/todos';
 import users from './api/users';
 
 function App() {
+
+  const allTodos =
+    todos.map((item, index) => (
+      {
+        id: item.id,
+        userId: item.userId,
+        title: item.title,
+        completed: item.completed,
+        users: users.find(user => user.id === item.userId)
+      }
+    ))
+  console.log(allTodos);
   return (
     <div className="App">
       <h1>Static list of todos</h1>
@@ -17,6 +29,28 @@ function App() {
         <span>Users: </span>
         {users.length}
       </p>
+
+      {allTodos.map((item, i) => (
+        // eslint-disable-next-line react/no-array-index-key
+        <div key={i}>
+          <div>
+            title
+            {item.users.name}
+          </div>
+        <div>
+          title
+          {item.title}
+        </div>
+      {item.completed ?
+        <input type="checkbox" checked onChange={() => true} />
+        :
+        <input type="checkbox" />
+      }
+        </div>
+      ))}
+
+
+
     </div>
   );
 }
